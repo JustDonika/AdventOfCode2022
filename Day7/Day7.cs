45a5cb140f2134cfe0b3ec838c2d26d675d8a17c
Donovan Tull <tulldono@ecs.vuw.ac.nz>
2022-12-08 13:40:51 +1300

    Two attempts made for Day 7; latter attempt successful

---
+++ Day7/Day7.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Csharp
+{
+    public class Node{
+        public List<Node> subdirectory = new List<Node>();
+        public string name = " ";
+        public int size = 0;
+        public int calculateSize(){
+            int sum = size;
+            for(int i = 0; i < subdirectory.Count; i++){
+                sum += subdirectory[i].calculateSize();
+            }
+            return sum;
+        }
+    }
+
+    public class Solution
+    {
+        public int partOne()
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            char[] delimiterChars = {' '};
+            Node origin = new Node();
+            Node system = new Node();
+            system.name = "/";
+            origin.subdirectory.Add(system);
+            Node operating = origin;
+            //List of subdirectories gone into
+            List<String> subdirs = new List<String>();
+
+            for(int i = 0; i < lines.Length; i++){
+                string line = lines[i];
+                Console.WriteLine(line);
+                Console.WriteLine(operating.name);
+                string[] factors = line.Split(delimiterChars);
+                //cd and not ..
+                if(factors[1].Equals("cd") && !factors[2].Equals("..")){
+                    for(int o = 0; o < operating.subdirectory.Count; o++){
+                        if(factors[2].Equals(operating.subdirectory.ElementAt(o).name)){
+                            operating = operating.subdirectory.ElementAt(o);
+                            subdirs.Add(factors[2]);
+                            o = 10000;
+                        }
+                    }
+                }
+                //cd and ..
+                else if(factors[1].Equals("cd")){ //Go back up a level.
+                    subdirs.RemoveAt(subdirs.Count - 1);
+                    operating = origin;
+                    for(int o = 0; o < subdirs.Count; o++){
+                        for(int u = 0; u < operating.subdirectory.Count; u++){
+                            if(factors[2].Equals(operating.subdirectory.ElementAt(u).name)){
+                                operating = operating.subdirectory.ElementAt(u);
+                                u = 10000;
+                        }
+                    }
+                    }
+                }
+                //Directory node
+                if(factors[0].Equals("dir")){
+                    Node n = new Node();
+                    n.name = factors[1];
+                    operating.subdirectory.Add(n);
+                }
+                double output = 0;
+                //Numeric node
+                if(double.TryParse(factors[0], out output)){
+                    Node n = new Node();
+                    n.size = Int32.Parse(factors[0]);
+                    operating.subdirectory.Add(n); 
+                }
+                
+            }
+            return 0;
+        }
+
+        public int partTwo()
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int answer = s.partOne();
+            Console.WriteLine(answer);
+            int answer2 = s.partTwo();
+            Console.WriteLine(answer2);
+        }
+    }
+}
---
+++ Day7Attempt2/Day7.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Csharp
+{
+    public class Node{
+        public List<String> subdirectory = new List<String>();
+        public int size = 0;
+
+    }
+
+    public class Solution
+    {
+
+        Dictionary<String, Node> allDirectories = new Dictionary<String, Node>();
+
+        public int calculateSize(string s){
+            Node n = allDirectories[s];
+            int sum = n.size;
+            for(int i = 0; i < n.subdirectory.Count; i++){
+                sum+=calculateSize(s+n.subdirectory.ElementAt(i));
+            }
+            return sum;
+        }
+
+        public int partOne()
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            char[] delimiterChars = {' '};
+            string currentDir = "";
+            allDirectories["/"] = new Node();
+            List<String> subdirs = new List<String>();
+
+            for(int i = 0; i < lines.Length; i++){
+                string line = lines[i];
+                string[] factors = line.Split(delimiterChars);
+                //cd and not ..
+                if(factors[1].Equals("cd") && !factors[2].Equals("..")){
+                    subdirs.Add(factors[2]);
+                    currentDir = "";
+                    for (int a = 0; a < subdirs.Count; a++){
+                        currentDir = currentDir + subdirs.ElementAt(a);
+                    }
+                }
+                //cd and ..
+                else if(factors[1].Equals("cd")){ //Go back up a level.
+                    subdirs.RemoveAt(subdirs.Count - 1);
+                    currentDir = "";
+                    for (int a = 0; a < subdirs.Count; a++){
+                        currentDir = currentDir + subdirs.ElementAt(a);
+                    }
+                }
+                //Directory node
+                else if(factors[0].Equals("dir")){
+                    allDirectories[currentDir].subdirectory.Add(factors[1]);
+                    allDirectories[currentDir+factors[1]] = new Node();
+                }
+                double output = 0;
+                //Numeric node
+                if(double.TryParse(factors[0], out output)){
+                    allDirectories[currentDir].size+=(int)output;
+                }
+            }
+            int sum = 0;
+            foreach(KeyValuePair<String, Node> entry in allDirectories)
+            {
+                int value = calculateSize(entry.Key);
+                if(value <= 100000){
+                    sum+=value;
+                }
+            }
+            return sum;
+        }
+
+        public int partTwo()
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            int minimumRequired = calculateSize("/")-40000000;
+            int nearestMinimum = 1000000000;
+            foreach(KeyValuePair<String, Node> entry in allDirectories)
+            {
+                int value = calculateSize(entry.Key);
+                if(value > minimumRequired && value < nearestMinimum){
+                    nearestMinimum = value;
+                }
+            }
+            Console.WriteLine(minimumRequired);
+            return nearestMinimum;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int answer = s.partOne();
+            Console.WriteLine(answer);
+            int answer2 = s.partTwo();
+            Console.WriteLine(answer2);
+        }
+    }
+}
